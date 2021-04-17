53db3cea2d92bb5d42e221d9b965d84888ded104
Aliona77 <chabanalona153@gmail.com>
2021-04-17 14:27:11 +0300

    вправи

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import './styles.scss';
+import "./styles/styles.scss";
 
-import sumNumbers from './utils';
+import sumNumbers from "./js/utils";
 
-console.log(sumNumbers (2, 3));
+import _ from "lodash";
+
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const btn = document.getElementById("sumBtn");
+
+const result = document.getElementById("result");
+
+btn.addEventListener("click", e => {
+  const num1 = Number(num1Input.value);
+  const num2 = Number(num2Input.value);
+  result.innerHTML = sumNumbers(num1, num2);
+});
